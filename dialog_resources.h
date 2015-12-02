a1ccf25bf72f9ccf42715eaaf1258832a6a15c99
Aleksander Pasiut <avertsec@gmail.com>
2015-12-02 13:32:51 +0100

    I forgot about this

--- dialog_resources.h
+++ dialog_resources.h
@@ -2,16 +2,8 @@
 
 #define CTRL_NUMB 100
 #define CTRL_EDIT 101
-#define CTRL_CHECKBOX 102
 
 #include <Windows.h>
 
 BOOL CALLBACK AddClockDialogProc(HWND, UINT, WPARAM, LPARAM);
 BOOL CALLBACK AddNandDialogProc(HWND, UINT, WPARAM, LPARAM);
-BOOL CALLBACK AddTextboxDialogProc(HWND, UINT, WPARAM, LPARAM);
-
-struct ADD_TEXTBOX_RV
-{
-	char text[256];
-	bool pointer;
-};
